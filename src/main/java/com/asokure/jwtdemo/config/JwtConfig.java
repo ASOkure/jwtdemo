f326de949ec38f095617ee1083a38e3a5d4a7c01
Akanimo Sam Okure <akan.okure@gmail.com>
2023-12-11 14:31:47 +0000

    add and implement jwtConfig userDetailsService and authentication manager

---
+++ src/main/java/com/asokure/jwtdemo/config/JwtConfig.java
@@ -0,0 +1,40 @@
+package com.asokure.jwtdemo.config;
+import com.asokure.jwtdemo.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+
+@Configuration
+@EnableWebSecurity
+public class JwtConfig extends WebSecurityConfigurerAdapter {
+
+
+@Autowired
+private CustomUserDetailService customUserDetailService;
+
+    // how we want to manage our authentication process
+@Override
+protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    auth.userDetailsService(customUserDetailService);
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+
+
+
+
+
+
+
+}
